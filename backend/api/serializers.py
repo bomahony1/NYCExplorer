403f05d866dc76431477cf4459e229afb82bf17d
bomahony1 <bomahony1999@gmail.com>
2023-06-25 21:46:37 +0100

    Created new backend due to error

--- backend/api/serializers.py
+++
@@ -1,9 +0,0 @@
-# Job of serializer is to convert model instances to JSON so the frontend can work with the recieved data easily
-
-from rest_framework import serializers
-from .models import attractions
-
-class attractionsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = attractions
-        fields = ('name', 'address')
