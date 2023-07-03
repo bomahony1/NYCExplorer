40277f606674c1f22e206259ce612294bf60e982
bomahony1 <bomahony1999@gmail.com>
2023-07-03 22:14:59 +0100

    Cleaned up file structure

--- backend/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from django.views.generic import TemplateView
-
-app_name = "website"
-
-urlpatterns = [
-    path("", TemplateView.as_view(template_name="website/index.html"))
-]
