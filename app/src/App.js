b97cc8eae23750a8fee9860abc170cb1812eadc1
bomahony1 <bomahony1999@gmail.com>
2023-07-03 22:18:59 +0100

    Placed endpoint in the frontend

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 class connectionExample extends React.Component {
   componentDidMount() {
-    const apiUrl = 'http://127.0.0.1:8000/api/';
+    // Complete the end point here e.g. api/hotels, api/events
+    const apiUrl = 'http://127.0.0.1:8000/api/restaurants';
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => console.log(data));
