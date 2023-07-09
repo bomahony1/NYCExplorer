14527645434a0035dd8d00fa024adaab1f0b5586
leslieruan <leslieruan8@gmail.com>
2023-07-09 23:44:09 +0100

    adjust the style of itineray and home

--- app/src/HomePage.js
+++ app/src/HomePage.js
@@ -1,37 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions, List, ListItemButton, ListItemText, ListSubheader, Collapse } from '@mui/material';
+import { Button,Card, CardActionArea, CardActions, List, ListItemButton, ListItemText, ListSubheader, Collapse } from '@mui/material';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-// import $ from 'jquery';
+
+
+function cards(imageUrl, title, time, description) {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia component="img" height="100" image={imageUrl} alt="" padding="10px" />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            {title}
+          </Typography>
+          <Typography gutterBottom variant="h7" component="div">
+            {time}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Bring me there
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
 
 // function NestedList() {
-//     const [open1, setOpen1] = useState(false);
-//     const [open2, setOpen2] = useState(false);
-//     const [open3, setOpen3] = useState(false);
-//     const [open4, setOpen4] = useState(false);
-  
-//     const handleClick1 = () => {
-//       setOpen1(!open1);
-//     };
-  
-//     const handleClick2 = () => {
-//       setOpen2(!open2);
-//     };
-  
-//     const handleClick3 = () => {
-//       setOpen3(!open3);
-//     };
-  
-//     const handleClick4 = () => {
-//       setOpen4(!open4);
-//     };
-  
+//   const [open1, setOpen1] = useState(false);
+//   const [open2, setOpen2] = useState(false);
+//   const [open3, setOpen3] = useState(false);
+//   const [open4, setOpen4] = useState(false);
 
-//     return (
+//   const handleClick1 = () => {
+//     setOpen1(!open1);
+//   };
+
+//   const handleClick2 = () => {
+//     setOpen2(!open2);
+//   };
+
+//   const handleClick3 = () => {
+//     setOpen3(!open3);
+//   };
+
+//   const handleClick4 = () => {
+//     setOpen4(!open4);
+//   };
+
+//   return (
 //     <List
 //       sx={{ width: '100%', maxWidth: 999, bgcolor: 'background.paper' }}
 //       component="nav"
@@ -52,7 +78,7 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 //           </ListItemButton>
 //         </List>
 //       </Collapse>
-      
+
 //       <ListItemButton onClick={handleClick2}>
 //         <ListItemText primary="Wall Street" />
 //         {open2 ? <ExpandLess /> : <ExpandMore />}
@@ -64,7 +90,7 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 //           </ListItemButton>
 //         </List>
 //       </Collapse>
-      
+
 //       <ListItemButton onClick={handleClick3}>
 //         <ListItemText primary="Time Square" />
 //         {open3 ? <ExpandLess /> : <ExpandMore />}
@@ -76,7 +102,7 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 //           </ListItemButton>
 //         </List>
 //       </Collapse>
-      
+
 //       <ListItemButton onClick={handleClick4}>
 //         <ListItemText primary="Statue of Liberty" />
 //         {open4 ? <ExpandLess /> : <ExpandMore />}
@@ -92,85 +118,12 @@ import ExpandMore from '@mui/icons-material/ExpandMore';
 //   );
 // }
 
-// function HomePage() {
-//   const [eventTitle, setEventTitle] = useState('');
-//   const [eventLocation, setEventLocation] = useState('');
-//   const [eventURL, setEventURL] = useState('');
-//   const [eventStartTime, setEventStartTime] = useState('');
-//   const [eventImage, setEventImage] = useState('');
-
-//   useEffect(() => {
-//     // Make the AJAX request
-//     $.ajax({
-//       type: 'GET',
-//       url: 'https://app.ticketmaster.com/discovery/v2/events.json?size=1&city=bronx&apikey=tpZiyonZeqnI84pouyaLdPeKLuwMQz4b',
-//       async: true,
-//       dataType: 'json',
-//       success: function (json) {
-//         console.log(json); // Print the API response to the console
-//         // Extract the event details from the response and update the component state
-//         const title = json?._embedded?.events?.[0]?.name || 'No event available';
-//         const location = json?._embedded?.events?.[0]?._embedded?.venues?.[0]?.name || 'Location not available';
-//         const url = json?._embedded?.events?.[0]?.url || '';
-//         const startTime = json?._embedded?.events?.[0]?.dates?.start?.localTime || '';
-//         const image = json?._embedded?.events?.[0]?.images?.[0]?.url || '';
-//         setEventTitle(title);
-//         setEventLocation(location);
-//         setEventURL(url);
-//         setEventStartTime(startTime);
-//         setEventImage(image);
-//       },
-//       error: function (xhr, status, err) {
-//         console.error(err);
-//       },
-//     });
-//   }, []);
-
-//   const handleBringMeThere = () => {
-//     window.open(eventURL, '_blank');
-//   };
-
-//   return (
-//     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-//       <div>
-//         <div style={{ textAlign: 'center', marginTop: '30px' }}>
-//           <Card>
-//             <CardActionArea>
-//               <CardMedia component="img" height="150" width="999" image={eventImage} alt="" padding="10px" />
-//               <CardContent>
-//                 <Typography gutterBottom variant="h5" component="div">
-//                   Today: {eventTitle}
-//                 </Typography>
-//                 <Typography variant="body2" color="text.secondary">
-//                   Example of headline related to Today at {eventLocation}<br />
-//                   Start Time: {eventStartTime}
-//                 </Typography>
-//               </CardContent>
-//             </CardActionArea>
-//             <CardActions>
-//               <Button size="small" color="primary" onClick={handleBringMeThere}>
-//                 Bring me there
-//               </Button>
-//             </CardActions>
-//           </Card>
-//         </div>
-//         <div style={{ marginTop: '30px' }}>
-//           <NestedList />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default HomePage;
-
-
-
 function NestedList() {
   const [open1, setOpen1] = useState(false);
   const [open2, setOpen2] = useState(false);
   const [open3, setOpen3] = useState(false);
   const [open4, setOpen4] = useState(false);
+  
 
   const handleClick1 = () => {
     setOpen1(!open1);
@@ -190,58 +143,55 @@ function NestedList() {
 
   return (
     <List
-      sx={{ width: '100%', maxWidth: 999, bgcolor: 'background.paper' }}
+      sx={{ width: '100%', maxWidth: 1888, bgcolor: 'background.paper' }}
       component="nav"
       aria-labelledby="nested-list-subheader"
-      subheader={
-        <ListSubheader component="div" id="nested-list-subheader">
-        </ListSubheader>
-      }
+      subheader={<ListSubheader component="div" id="nested-list-subheader"></ListSubheader>}
     >
       <ListItemButton onClick={handleClick1}>
-        <ListItemText primary="Empire State Building" />
+        <ListItemText primary="Empire State Building" style={{ textAlign: 'center' }} />
         {open1 ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
       <Collapse in={open1} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="Starred" />
+            {cards('imageUrl1', 'Title 1', 'Time 1', 'Description 1')}
           </ListItemButton>
         </List>
       </Collapse>
 
       <ListItemButton onClick={handleClick2}>
-        <ListItemText primary="Wall Street" />
+        <ListItemText primary="Wall Street" style={{ textAlign: 'center' }}/>
         {open2 ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
       <Collapse in={open2} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="Starred" />
+            {cards('imageUrl2', 'Title 2', 'Time 2', 'Description 2')}
           </ListItemButton>
         </List>
       </Collapse>
 
       <ListItemButton onClick={handleClick3}>
-        <ListItemText primary="Time Square" />
+        <ListItemText primary="Time Square" style={{ textAlign: 'center' }}/>
         {open3 ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
       <Collapse in={open3} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="Starred" />
+            {cards('imageUrl3', 'Title 3', 'Time 3', 'Description 3')}
           </ListItemButton>
         </List>
       </Collapse>
 
       <ListItemButton onClick={handleClick4}>
-        <ListItemText primary="Statue of Liberty" />
+        <ListItemText primary="Statue of Liberty"style={{ textAlign: 'center' }} />
         {open4 ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
       <Collapse in={open4} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
           <ListItemButton sx={{ pl: 4 }}>
-            <ListItemText primary="Starred" />
+            {cards('imageUrl4', 'Title 4', 'Time 4', 'Description 4')}
           </ListItemButton>
         </List>
       </Collapse>
@@ -250,70 +200,58 @@ function NestedList() {
 }
 
 function HomePage() {
-  const [eventTitle, setEventTitle] = useState('');
-  const [eventLocation, setEventLocation] = useState('');
-  const [eventURL, setEventURL] = useState('');
-  const [eventStartTime, setEventStartTime] = useState('');
-  const [eventImage, setEventImage] = useState('');
+  const [events, setEvents] = useState([]);
+  const [activeSlide, setActiveSlide] = useState(0);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/events/') // Fetch data from the backend server
+    fetch('http://127.0.0.1:8000/api/events/')
       .then(response => response.json())
       .then(data => {
         if (data.length > 0) {
-          const event = data[0];
-          setEventTitle(event.name);
-          setEventLocation(event.latitude);
-          setEventURL(event.url);
-          setEventStartTime(event.time);
-          setEventImage(event.image);
+          setEvents(data.slice(0, 5)); // Only store the first 5 events
         } else {
-          setEventTitle('No event available');
-          setEventLocation('Location not available');
-          setEventURL('');
-          setEventStartTime('');
-          setEventImage('');
+          setEvents([]);
         }
       })
       .catch(error => {
         console.error(error);
-        setEventTitle('No event available');
-        setEventLocation('Location not available');
-        setEventURL('');
-        setEventStartTime('');
-        setEventImage('');
+        setEvents([]);
       });
   }, []);
 
-  const handleBringMeThere = () => {
-    window.open(eventURL, '_blank');
+  const handleBringMeThere = (url) => {
+    window.open(url, '_blank');
   };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div>
         <div style={{ textAlign: 'center', marginTop: '30px' }}>
-          <Card>
-            <CardActionArea>
-              <CardMedia component="img" height="150" width="999" image={eventImage} alt="" padding="10px" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Today: {eventTitle}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Example of headline related to Today at {eventLocation}<br />
-                  Start Time: {eventStartTime}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary" onClick={handleBringMeThere}>
-                Bring me there
-              </Button>
-            </CardActions>
-          </Card>
+          <Carousel selectedItem={activeSlide} onChange={setActiveSlide}>
+            {events.map((event, index) => (
+              <div onMouseEnter={() => setActiveSlide(index)}>
+                <CardActionArea>
+                  <CardMedia component="img" height="200" width="699" image={event.image} alt="" padding="10px" />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {event.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Throw in at {event.date}<br />
+                      Start Time: {event.time}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary" onClick={() => handleBringMeThere(event.url)}>
+                    Bring me there
+                  </Button>
+                </CardActions>
+              </div>
+            ))}
+          </Carousel>
         </div>
-        <div style={{ marginTop: '30px' }}>
+        <div style={{ marginTop: '10px' }}>
           <NestedList />
         </div>
       </div>
--- app/src/ItineraryPage.js
+++ app/src/ItineraryPage.js
@@ -1,108 +1,226 @@
-import React from "react";
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardActions from '@mui/material/CardActions';
+import { useState, useRef } from "react";
+import { motion, AnimatePresence, useScroll, useSpring} from "framer-motion";
+import { Button, Box, ButtonGroup } from "@mui/material";
+// import Card from '@mui/material/Card';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+// import Typography from '@mui/material/Typography';
+// import CardActionArea from '@mui/material/CardActionArea';
+// import CardActions from '@mui/material/CardActions';
+import "./Itinerary.css"
+import { removeItem } from "./array.ts";
 
-function Buttons(){
-    const buttons = [
-        <Button key="one">1-3 Days</Button>,
-        <Button key="two">4-6 Days</Button>,
-        <Button key="three">7-10 Days</Button>,
-      ];
-    return (
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            '& > *': {
-              m: 2,
-            },
-          }}
-        >
-          <ButtonGroup size="large" aria-label="large button group">
-            {buttons}
-          </ButtonGroup>
-        </Box>
-      );
+
+// function Buttons(){
+//     const buttons = [
+//         <Button key="one">1-3 Days</Button>,
+//         <Button key="two">4-6 Days</Button>,
+//         <Button key="three">7-10 Days</Button>,
+//       ];
+//     return (
+//         <Box
+//           sx={{
+//             display: 'flex',
+//             flexDirection: 'column',
+//             alignItems: 'center',
+//             '& > *': {
+//               m: 2,
+//             },
+//           }}
+//         >
+//           <ButtonGroup size="large" aria-label="large button group">
+//             {buttons}
+//           </ButtonGroup>
+//         </Box>
+//       );
+// }
+
+function Buttons({ setSelectedTab }) {
+  const buttons = [
+    { label: "1-3 Days", content: "Content for 1-3 Days" },
+    { label: "4-6 Days",  content: "Content for 4-6 Days" },
+    { label: "7-10 Days", content: "Content for 7-10 Days" },
+  ];
+  const handleClick = (item) => {
+    setSelectedTab(item.content);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        "& > *": {
+          m: 2,
+        },
+      }}
+    >
+      <ButtonGroup size="large" aria-label="large button group">
+        {buttons.map((item) => (
+          <Button
+            key={item.label}
+            onClick={() => handleClick(item)}
+          >
+           {item.label}
+          </Button>
+        ))}
+      </ButtonGroup>
+    </Box>
+  );
 }
 
-function cards(imageUrl, title,time, description) {
-    return (
-      <div>
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="100"
-              image={imageUrl}
-              alt=""
-              padding="10px"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h6" component="div">
-                {title}
-              </Typography>
-              <Typography gutterBottom variant="h7" component="div">
-                {time}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {description}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Bring me there
-            </Button>
-          </CardActions>
-        </Card>
-      </div>
-    );
-  }
-function ItineraryPage() {
-    return (
+function Window({ content }) {
+  return (
+    <div className="window">
+       <div className="window-content">
+      {content === "Content for 1-3 Days" && (
         <div>
+          <h3>Gallery for 1-3 Days</h3>
+          {/* Your gallery component goes here */}
+          <p>Additional text or description for 1-3 Days</p>
+        </div>
+      )}
+      {content === "Content for 4-6 Days" && (
+        <div>
+          <h3>Gallery for 4-6 Days</h3>
+          {/* Your gallery component goes here */}
+          <p>Additional text or description for 4-6 Days</p>
+        </div>
+      )}
+      {content === "Content for 7-10 Days" && (
+        <div>
+          <h3>Gallery for 7-10 Days</h3>
+          {/* Your gallery component goes here */}
+          <p>Additional text or description for 7-10 Days</p>
+        </div>
+      )}
+      </div>
+    </div>
+  );
+}
+
+
+
+
+// function cards(imageUrl, title,time, description) {
+//     return (
+//       <div>
+//         <Card>
+//           <CardActionArea>
+//             <CardMedia
+//               component="img"
+//               height="100"
+//               image={imageUrl}
+//               alt=""
+//               padding="10px"
+//             />
+//             <CardContent>
+//               <Typography gutterBottom variant="h6" component="div">
+//                 {title}
+//               </Typography>
+//               <Typography gutterBottom variant="h7" component="div">
+//                 {time}
+//               </Typography>
+//               <Typography variant="body2" color="text.secondary">
+//                 {description}
+//               </Typography>
+//             </CardContent>
+//           </CardActionArea>
+//           <CardActions>
+//             <Button size="small" color="primary">
+//               Bring me there
+//             </Button>
+//           </CardActions>
+//         </Card>
+//       </div>
+//     );
+//   }
+
+
+
+function Pop(){
+  const count = useRef(0);
+  const [items, setItems] = useState([0]);
+  const [popLayout, setPopLayout] = useState(false);
+
+  return (
+    <div className="example">
+      <div className="controls">
+        <label className="enable">
+          <code>POP YOUR Itinerary </code>
+          <input
+            type="checkbox"
+            checked={popLayout}
+            onChange={(e) => setPopLayout(e.currentTarget.checked)}
+          />
+        </label>
+        <motion.button
+          whileTap={{ scale: 0.95 }}
+          onClick={() => {
+            count.current++;
+            setItems([...items, count.current]);
+          }}
+        >
+          Add item
+        </motion.button>
+      </div>
+      <ul>
+        <AnimatePresence mode={popLayout ? "popLayout" : "sync"}>
+          {items.map((id) => (
+            <motion.li
+              layout
+              // initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.8, opacity: 0 }}
+              transition={{ type: "spring" }}
+              key={id}
+              onClick={() => {
+                const newItems = [...items];
+                removeItem(newItems, id);
+                setItems(newItems);
+              }}
+            />
+          ))}
+        </AnimatePresence>
+      </ul>
+    </div>
+  );
+}
+
+function ItineraryPage() {
+  const { scrollYProgress } = useScroll();
+  const [selectedTab, setSelectedTab] = useState(null);
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 200,
+    damping: 30,
+    restDelta: 0.001
+  });
+
+
+    return (
+        <div className="itinerary-page">
             <div>
                 <h2>How many days are you planning to stay?</h2>
-                <Buttons />
+                <Buttons setSelectedTab={setSelectedTab} />
+                <Window content={selectedTab} />
             </div>
-            <div>
-            <h1>New York Day 1</h1>
+            
+            <div className="pop">
+            <Pop />
             </div>
-            <div>
-            {cards(
-        'https://example.com/image1.jpg',
-        'Breakfast',
-        '9:00 AM - 9:30 AM',
-        'Example description for card 1'
-      )}
-      {cards(
-        'https://example.com/image2.jpg',
-        'Attractions 1',
-        '9:30 AM - 11:30 AM',
-        'Example description for card 2'
-      )}
-      {cards(
-        'https://example.com/image1.jpg',
-        'Attractions 2',
-        '11:30 AM - 1:30 PM',
-        'Example description for card 1'
-      )}
-      {cards(
-        'https://example.com/image2.jpg',
-        'Lunch',
-        '1:30 PM - 2:30 PM',
-        'Example description for card 2'
-      )}
-            </div>
-        </div>
+            
+            <article>
+      <motion.div className="progress" style={{ scaleX }} />
+      <motion.div
+        initial={{ scaleX: 1 }}
+        animate={{ scaleX: 0, transition: { duration: 0.5, ease: "circOut" } }}
+        exit={{ scaleX: 1, transition: { duration: 0.5, ease: "circIn" } }}
+        style={{ originX: true ? 0 : 1 }}
+        className="privacy-screen"
+      />
+    </article>
+      </div>
     );
 }
 
