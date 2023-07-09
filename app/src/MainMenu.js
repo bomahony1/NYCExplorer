4c247f2875fd3fb704bb62bc4f6f2d1b759b7256
leslieruan <leslieruan8@gmail.com>
2023-07-10 00:49:36 +0100

    change the web color

--- app/src/MainMenu.js
+++ app/src/MainMenu.js
@@ -27,20 +27,34 @@ export default function MainMenu() {
   }
 
   return (
-    <Box sx={{ margin: '10px' }}>
+    <Box >
       <Tabs
         value={value}
         onChange={handleChange}
         indicatorColor="primary"
         textColor="primary"
         centered
+        sx={{
+          boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.1)' // Add shadow effect to the border
+        }}
       >
-        <Tab label="Home" icon={<HomeOutlinedIcon />} />
-        <Tab label="Map" icon={<SearchIcon />} />
-        <Tab label="Itinerary" icon={<PersonPinIcon />} />
+        <Tab
+          label="Home"
+          icon={<HomeOutlinedIcon />}
+          sx={{ color: '#1C2541' }} // Customize the font color of the tab
+        />
+        <Tab
+          label="Map"
+          icon={<SearchIcon />}
+          sx={{ color: '#1C2541' }} // Customize the font color of the tab
+        />
+        <Tab
+          label="Itinerary"
+          icon={<PersonPinIcon />}
+          sx={{ color: '#1C2541' }} // Customize the font color of the tab
+        />
       </Tabs>
       <Box sx={{ bgcolor: 'white' }}>{content}</Box>
     </Box>
   );
 }
-
