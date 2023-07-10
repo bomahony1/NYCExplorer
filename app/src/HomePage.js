b4eec13f8ff960c40b06f3d9a68a0a669a213d33
leslieruan <leslieruan8@gmail.com>
2023-07-10 15:19:33 +0100

    add icon and slogan

--- app/src/HomePage.js
+++ app/src/HomePage.js
@@ -7,116 +7,85 @@ import Typography from '@mui/material/Typography';
 import { Button,Card, CardActionArea, CardActions, List, ListItemButton, ListItemText, ListSubheader, Collapse } from '@mui/material';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
+import { motion, AnimatePresence } from 'framer-motion';
 
+const itemVariants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 }
+  },
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
+};
+
+// function cards(imageUrl, title, time, description) {
+//   return (
+//     <Card>
+//       <CardActionArea>
+//         <CardMedia component="img" height="100" image={imageUrl} alt="" padding="10px" />
+//         <CardContent>
+//           <Typography gutterBottom variant="h6" component="div">
+//             {title}
+//           </Typography>
+//           <Typography gutterBottom variant="h7" component="div">
+//             {time}
+//           </Typography>
+//           <Typography variant="body2" color="text.secondary">
+//             {description}
+//           </Typography>
+//         </CardContent>
+//       </CardActionArea>
+//       <CardActions>
+//         <Button size="small" color="primary">
+//           Bring me there
+//         </Button>
+//       </CardActions>
+//     </Card>
+//   );
+// }
 
 function cards(imageUrl, title, time, description) {
   return (
-    <Card>
-      <CardActionArea>
-        <CardMedia component="img" height="100" image={imageUrl} alt="" padding="10px" />
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="div">
-            {title}
-          </Typography>
-          <Typography gutterBottom variant="h7" component="div">
-            {time}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Bring me there
-        </Button>
-      </CardActions>
-    </Card>
+    <motion.div
+    variants={itemVariants}
+    initial="closed"
+    animate="open"
+    exit="closed"
+    style={{
+      overflow: 'hidden',
+      backgroundColor: 'var(--accent)',
+      color: 'var(--background)',
+      borderRadius: '10px',
+      padding: '10px',
+      marginBottom: '10px',
+    }}
+    >
+      <Card>
+        <CardActionArea>
+          <CardMedia component="img" height="100" image={imageUrl} alt="" padding="10px" />
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              {title}
+            </Typography>
+            <Typography gutterBottom variant="h7" component="div">
+              {time}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Bring me there
+          </Button>
+        </CardActions>
+      </Card>
+    </motion.div>
   );
 }
 
-// function NestedList() {
-//   const [open1, setOpen1] = useState(false);
-//   const [open2, setOpen2] = useState(false);
-//   const [open3, setOpen3] = useState(false);
-//   const [open4, setOpen4] = useState(false);
 
-//   const handleClick1 = () => {
-//     setOpen1(!open1);
-//   };
-
-//   const handleClick2 = () => {
-//     setOpen2(!open2);
-//   };
-
-//   const handleClick3 = () => {
-//     setOpen3(!open3);
-//   };
-
-//   const handleClick4 = () => {
-//     setOpen4(!open4);
-//   };
-
-//   return (
-//     <List
-//       sx={{ width: '100%', maxWidth: 999, bgcolor: 'background.paper' }}
-//       component="nav"
-//       aria-labelledby="nested-list-subheader"
-//       subheader={
-//         <ListSubheader component="div" id="nested-list-subheader">
-//         </ListSubheader>
-//       }
-//     >
-//       <ListItemButton onClick={handleClick1}>
-//         <ListItemText primary="Empire State Building" />
-//         {open1 ? <ExpandLess /> : <ExpandMore />}
-//       </ListItemButton>
-//       <Collapse in={open1} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItemButton sx={{ pl: 4 }}>
-//             <ListItemText primary="Starred" />
-//           </ListItemButton>
-//         </List>
-//       </Collapse>
-
-//       <ListItemButton onClick={handleClick2}>
-//         <ListItemText primary="Wall Street" />
-//         {open2 ? <ExpandLess /> : <ExpandMore />}
-//       </ListItemButton>
-//       <Collapse in={open2} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItemButton sx={{ pl: 4 }}>
-//             <ListItemText primary="Starred" />
-//           </ListItemButton>
-//         </List>
-//       </Collapse>
-
-//       <ListItemButton onClick={handleClick3}>
-//         <ListItemText primary="Time Square" />
-//         {open3 ? <ExpandLess /> : <ExpandMore />}
-//       </ListItemButton>
-//       <Collapse in={open3} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItemButton sx={{ pl: 4 }}>
-//             <ListItemText primary="Starred" />
-//           </ListItemButton>
-//         </List>
-//       </Collapse>
-
-//       <ListItemButton onClick={handleClick4}>
-//         <ListItemText primary="Statue of Liberty" />
-//         {open4 ? <ExpandLess /> : <ExpandMore />}
-//       </ListItemButton>
-//       <Collapse in={open4} timeout="auto" unmountOnExit>
-//         <List component="div" disablePadding>
-//           <ListItemButton sx={{ pl: 4 }}>
-//             <ListItemText primary="Starred" />
-//           </ListItemButton>
-//         </List>
-//       </Collapse>
-//     </List>
-//   );
-// }
 
 function NestedList() {
   const [open1, setOpen1] = useState(false);
