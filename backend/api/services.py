25a012aed78a93bd7b27c09575366c25e9d2d2e8
bomahony1 <bomahony1999@gmail.com>
2023-07-16 21:33:46 +0100

    added tourist attraction data

--- backend/api/services.py
+++ backend/api/services.py
@@ -55,6 +55,7 @@ def get_venues_restaurant():
         "open_now": "true",
         "categoryId": "4d4b7105d754a06374d81259",  # Category ID for Food (Restaurants)
         "limit": 50,  # Number of results to retrieve per request
+        "richData": "true"  # Include rich data for POIs
     }
 
     try:
@@ -199,5 +200,56 @@ def get_google_restaurants():
 
     return restaurant_data
 
+def get_google_attractions():
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+    api_key = "AIzaSyDgYC8VXvS4UG9ApSUhS2v-ByddtHljFls"
+    params = {
+        "query": "tourist attractions in Manhattan, New York",
+        "key": api_key,
+        "fields": "name,formatted_address,geometry/location,rating,photos"
+    }
+    attraction_data = []
 
+    try:
+        while True:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            data = response.json()
+            results = data["results"]
+
+            for result in results:
+                name = result["name"]
+                address = result["formatted_address"]
+                location = result["geometry"]["location"]
+                lat = location["lat"]
+                lng = location["lng"]
+                rating = result.get("rating")
+                photos = result.get("photos")
+
+                photo_urls = []
+                if photos:
+                    for photo in photos:
+                        photo_reference = photo.get("photo_reference")
+                        photo_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_reference}&key={api_key}"
+                        photo_urls.append(photo_url)
+
+                attraction_data.append({
+                    "name": name,
+                    "address": address,
+                    "latitude": lat,
+                    "longitude": lng,
+                    "rating": rating,
+                    "photos": photo_urls
+                })
+
+            if "next_page_token" not in data:
+                break
+
+            params["pagetoken"] = data["next_page_token"]
+            time.sleep(2)  # Delay between API calls as per Google's guidelines
+
+    except requests.exceptions.RequestException as e:
+        print("Error in get_attractions:", e)
+        
+    return attraction_data
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from django.core.cache import cache
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 import requests
-from .services import get_venues_restaurant, get_venues_hotels, get_weather, get_events, get_google_restaurants
+from .services import get_venues_restaurant, get_venues_hotels, get_weather, get_events, get_google_restaurants, get_google_attractions
 
 class WeatherAPIView(generics.GenericAPIView):
     @method_decorator(cache_page(60 * 15))  # Cache the response for 15 minutes
@@ -37,6 +37,26 @@ class GoogleRestaurantAPIView(generics.ListAPIView):
         cache.set(self.cache_key, google_restaurant_data, timeout=3600)  # Cache for 1 hour
 
         return Response(google_restaurant_data)
+
+class GoogleAttractionsAPIView(generics.ListAPIView):
+    queryset = []  # Add your queryset here if you have one
+    cache_key = 'google_attraction_data'
+
+    def get_queryset(self):
+        # You can implement custom queryset logic here if needed
+        return self.queryset
+
+    def get(self, request, *args, **kwargs):
+        # Check if the data is already cached
+        cached_data = cache.get(self.cache_key)
+        if cached_data is not None:
+            return Response(cached_data)
+
+        # If not cached, fetch the data and cache it
+        google_attraction_data = get_google_attractions()
+        cache.set(self.cache_key, google_attraction_data, timeout=3600)  # Cache for 1 hour
+
+        return Response(google_attraction_data)
 
 
 class HotelsAPIView(generics.GenericAPIView):
