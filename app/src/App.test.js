a0a63afab6f6cc96590cc555ac8039da6d527663
paul-od24 <117522367+paul-od24@users.noreply.github.com>
2023-08-01 16:16:55 +0100

    Recommend test

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -7,3 +7,25 @@ test('renders the App component', () => {
   const learnReactElement = screen.getByText(/Learn React/i);
   expect(learnReactElement).toBeInTheDocument();
 });
+
+test('renders the Welcome component during the animation', () => {
+  render(<App />);
+  const welcomeElement = screen.getByText('Welcome');
+  expect(welcomeElement).toBeInTheDocument();
+});
+
+test('renders the MainMenu component after the animation', () => {
+  render(<App />);
+  const mainMenuElement = screen.getByText('Main Menu');
+  expect(mainMenuElement).toBeInTheDocument();
+});
+
+test('hides animation elements after the animation is completed', () => {
+  render(<App />);
+  const animationDuration = 2000; // Match the duration used in useEffect of App.js
+  jest.advanceTimersByTime(animationDuration);
+  const elements = screen.queryAllByTestId('mojs-shape');
+  elements.forEach((element) => {
+    expect(element).not.toBeVisible();
+  });
+});
