15d0aa2210d050721147a4640bc058de76ae0aa4
paul-od24 <117522367+paul-od24@users.noreply.github.com>
2023-08-05 11:55:11 +0100

    frontend testing1

--- app/jest.config.js
+++ app/jest.config.js
@@ -6,10 +6,16 @@ module.exports = {
   },
   transformIgnorePatterns: [
     '/node_modules/(?!(framer-motion-3d)/)',
+    "/node_modules/(?!react-merge-refs)/",
   ],
   globals: {
     'ts-jest': {
       isolatedModules: true,
     },
   },
+
+  extensionsToTreatAsEsm: ['.mjs'],
+  testMatch: ['**/*.test.js', '**/*.test.mjs'],
+  moduleFileExtensions: ['js', 'mjs', 'jsx', 'json', 'node'],
+  esModuleInterop: true,
 };
