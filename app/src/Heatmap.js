0afa1c7c5c80b96c0f9feb08a649a0f367000291
leslieruan <leslieruan8@gmail.com>
2023-08-05 12:49:21 +0100

    fix button of heatmap

--- app/src/Heatmap.js
+++ app/src/Heatmap.js
@@ -164,11 +164,54 @@ const Heatmap = ({ onHeatmapDataReceived, heatmapVisible, onToggleHeatmap, polyg
   }, [heatmapVisible]);
 
   const fetchHeatmapData = (selectedDate) => {
-    // ... Fetch heatmap data ...
+    // Format the selected date to match the API's query string format
+    const formData = {
+      hour: selectedDate.format("H"), // Get the hour in 24-hour format
+      day: selectedDate.date(),
+      month: selectedDate.month() + 1,
+    };
+
+    const apiUrl = 'http://127.0.0.1:8000/api/heatMap/';
+    const queryParams = new URLSearchParams(formData).toString();
+
+    fetch(apiUrl + '?' + queryParams)
+      .then((response) => response.json())
+      .then((data) => {
+        const polygonsArray = data.prediction.map((zoneData) => {
+            const latLngs = zoneData.coordinates.map((coordinate) => ({
+                lat: coordinate[0],
+                lng: coordinate[1],
+              }));
+            const prediction = zoneData.prediction;
+            const color = getColorBasedOnPrediction(prediction);
+            const zoneNumber = zoneData.zoneNumber;
+            return { zoneNumber,latLngs, color, prediction  };
+          });
+        //   setPolygons(polygonsArray); // Update the polygons state with the fetched data
+          onHeatmapDataReceived(polygonsArray);// You can pass the polygons to the parent if needed.
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
+
   const getColorBasedOnPrediction = (prediction) => {
-    // ... Color logic ...
+    if (prediction === -1.5) {
+      return 'rgba(220, 218, 216, 0)'; // Weight 0: Transparent
+    } else if (prediction <= -1) {
+      return 'rgba(180, 223, 187, 1)'; // Weight 1-60: Light green
+    } else if (prediction <= -0.5) {
+      return 'rgba(216, 209, 224, 1)'; // Weight 61-150: Light purple
+    } else if (prediction <= 0) {
+      return 'rgba(246, 244, 198, 1)'; // Weight 151-300: Light yellow
+    } else if (prediction <= 0.5) {
+      return 'rgba(246, 217, 190, 1)'; // Weight 301-450: Light orange
+    } else if (prediction <= 1) {
+      return 'rgba(158, 185, 215, 1)'; // Weight 451-600: Light blue
+    } else {
+      return 'rgba(253, 136, 194, 1)'; // Weight > 600: Light pink
+    }
   };
 
   return (
